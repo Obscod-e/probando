34f29ec045198a6f64d297503db3d7912c7e15b2
Obscod-e <asilvaco@uoc.edu>
2021-01-19 19:32:55 +0100

    Otro commit

--- ejemplo.swift
+++ ejemplo.swift
@@ -3,3 +3,6 @@
 print("Hola Mundo, mi primer commit!")
 
 // Parece que el primer commit ha funcionado.
+
+// Esto está genial, ya puedo hacer commits, pushear el código y tenerlo todo subido en github.
+
